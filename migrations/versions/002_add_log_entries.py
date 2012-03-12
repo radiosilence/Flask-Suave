f915c708de47d9f95cec7ccaedc186a058342815
James Cleveland <jamescleveland@gmail.com>
2012-03-12 00:20:48 +0000

    fixed log migrations

--- migrations/versions/002_add_log_entries.py
+++ migrations/versions/002_add_log_entries.py
@@ -1,13 +1,24 @@
 from sqlalchemy import *
 from migrate import *
 
+meta = MetaData()
+
+log_entry = Table(
+    'suave_log_entry', meta,
+    Column('id', Integer, primary_key=True),
+    Column('subject_id', Integer, ForeignKey('suave_user.id')),
+    Column('target_id', Integer),
+    Column('verb', String(255)),
+    Column('when', DateTime),
+    Column('class_name', String(255))
+)
 
 def upgrade(migrate_engine):
-    # Upgrade operations go here. Don't create your own engine; bind
-    # migrate_engine to your metadata
-    pass
+    meta.bind = migrate_engine
+    user = Table('suave_user', meta, autoload=True)
+    log_entry.create()
 
 
 def downgrade(migrate_engine):
-    # Operations to reverse the above upgrade go here.
-    pass
+    meta.bind = migrate_engine
+    log_entry.drop()
