9e4fc8d5aae7e5b67450f3ac10da48344e0adda0
James Cleveland <jamescleveland@gmail.com>
2012-03-12 00:22:11 +0000

    ..

--- suave.py
+++ suave.py
@@ -14,22 +14,16 @@ def create(app_name):
 
 
 def _migrate():
-    def run_on(repo, db_url):
-        migrate_main(url=db_url, debug='False', repository=repo, name=repo)
-
 
     from app import app
 
     db_url = app.config['SQLALCHEMY_DATABASE_URI']
 
-    if sys.argv[1] == 'suave':
-        repo = 'migrations_suave'
-    elif sys.argv[1] == 'local':
-        repo = 'migrations'
-    else:
-        exit("You must specify migrations as either suave or local.")
+    repo = 'migrations_%s' % sys.argv[1]
+    if not os.path.exists(repo) and sys.argv[2] != 'create':
+        exit("You must specify a valid migration repo.")
     del sys.argv[1]
-    run_on(repo, db_url)
+    migrate_main(url=db_url, debug=False, repository=repo, name=repo)
 
 def main():
     try:
